b8de4e1320487fc04b6c1057f6029103a15ce345
Madonna Adel <madonnadel0@gmail.com>
2024-08-11 08:49:37 +0300

    created Angular project

--- physicaltherapyfront/src/app/app.component.ts
+++ physicaltherapyfront/src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], 
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [NgbModal] 
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'physicaltherapyfront';
-
 }
