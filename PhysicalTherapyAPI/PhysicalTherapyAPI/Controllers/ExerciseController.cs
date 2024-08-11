33efec7f37c94e71ad0eec479f587c383b7871aa
marian <marian.rashed23@gmail.com>
2024-08-11 18:26:26 +0300

    changes

--- PhysicalTherapyAPI/PhysicalTherapyAPI/Controllers/ExerciseController.cs
+++ PhysicalTherapyAPI/PhysicalTherapyAPI/Controllers/ExerciseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PhysicalTherapyAPI.DTOs;
 using PhysicalTherapyAPI.Models;
@@ -11,10 +12,12 @@ namespace PhysicalTherapyAPI.Controllers
     public class ExerciseController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public ExerciseController(IUnitOfWork unitOfWork)
+        public ExerciseController(IUnitOfWork unitOfWork,IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         [HttpGet("GetExercisesByCategoryId")]
@@ -30,6 +33,33 @@ namespace PhysicalTherapyAPI.Controllers
                 return Ok(_unitOfWork.ExerciseRepository.GetFilteredExercises(filterObj, "Category"));
             }
             return Ok(ModelState);
+        }
+
+        [HttpPost("AddExercise")]
+        public IActionResult AddExercise([FromQuery]ExerciseDTO exerciseDTO, IFormFile img)
+        {
+            if (ModelState.IsValid)
+            {
+                if (img == null || img.Length == 0)
+                { return BadRequest("No file uploaded."); }
+
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images", "Exercise");
+                var fileName = Guid.NewGuid().ToString() + "_" + img.FileName;
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    img.CopyTo(stream);
+                }
+                var photoUrl = $"https://localhost:7197/Images/Exercise/{fileName}";
+                Exercise exercise = _mapper.Map<Exercise>(exerciseDTO);
+                exercise.PhotoUrl = photoUrl;
+                _unitOfWork.ExerciseRepository.Add(exercise);
+                _unitOfWork.save();
+                return Ok();
+            }
+            return BadRequest();
+
         }
     }
 }
